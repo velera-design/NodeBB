7be6515160ed8e5ce5c0e84402bf9c7ea9f03129
Justin Moore <justin.moore@coop.org>
2025-11-25 12:01:43 -0500

    get protocol on origin for CSRFsync

--- src/middleware/csrf.js
+++ src/middleware/csrf.js
@@ -1,15 +1,11 @@
-'use strict';
+"use strict";
 
-const { csrfSync } = require('csrf-sync');
+const { csrfSync } = require("csrf-sync");
 
-const {
-	generateToken,
-	csrfSynchronisedProtection,
-	isRequestValid,
-} = csrfSync({
+const { generateToken, csrfSynchronisedProtection, isRequestValid } = csrfSync({
 	getTokenFromRequest: (req) => {
-		if (req.headers['x-csrf-token']) {
-			return req.headers['x-csrf-token'];
+		if (req.headers["x-csrf-token"]) {
+			return req.headers["x-csrf-token"];
 		} else if (req.body && req.body.csrf_token) {
 			return req.body.csrf_token;
 		} else if (req.body && req.body._csrf) {
@@ -17,6 +13,12 @@ const {
 		} else if (req.query && req.query._csrf) {
 			return req.query._csrf;
 		}
+	},
+	getOrigin: (req) => {
+		const proto = req.headers["x-forwarded-proto"] || req.protocol;
+		console.log(proto);
+
+		return `${proto}://${req.get("host")}`;
 	},
 	size: 64,
 });
